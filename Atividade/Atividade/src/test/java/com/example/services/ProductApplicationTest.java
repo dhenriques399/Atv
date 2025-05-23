b15fb206f28c9b33f9da1dedcd9acfbe7a6d9d22
dhenriques399 <diegohenriques399@gmail.com>
2025-05-23 12:21:22 -0300

    23/05/25
    atualizado

---
+++ Atividade/Atividade/src/test/java/com/example/services/ProductApplicationTest.java
@@ -0,0 +1,48 @@
+package com.example.services;
+
+import com.example.applications.ProductApplication;
+import com.example.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+
+class ProductApplicationTest {
+        Product product;
+
+        @InjectMocks // Foco do teste, nesse caso a classe ProductApplication.
+        ProductApplication productApplication;
+
+        @Mock // eu to focado na classe ProductApplocation são as dependencias que eu quero "mocar"
+        ProductService productService;
+
+        @BeforeEach
+        public void setUp() {
+            product  = new Product(1, "x-bacon", 100, "x-bacon.jpg");
+        }
+
+        @Test
+        public void testarSalvarImgCorretamente() {
+            productService.save(product);
+            Mockito.verify(productService).save(product);
+            Assertions.assertEquals("x-bacon.jpg", product.getImage());
+        }
+        @Test
+        public void testarRemoverImgCorretamente() {
+            productService.remove(product.getId());
+            Mockito.verify(productService).remove(product.getId());
+            Assertions.assertEquals("x-bacon.jpg", product.getImage());
+        }
+        @Test
+        public void testarAtualizarImgCorretamente() {
+            productService.update(product);
+            Mockito.verify(productService).update(product);
+            Assertions.assertEquals("x-bacon.jpg", product.getImage());
+        }
+    }
